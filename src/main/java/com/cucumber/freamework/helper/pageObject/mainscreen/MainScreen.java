e76cb092cd139b3626ba92cb036d2a0f07a2d41f
Vaibhav Bhapkar <vaibhav.bhapkar@corp.smtsoftware.com>
2019-02-18 11:09:15 +0100

    MainScreen.java updated

--- src/main/java/com/cucumber/freamework/helper/pageObject/mainscreen/MainScreen.java
+++ src/main/java/com/cucumber/freamework/helper/pageObject/mainscreen/MainScreen.java
@@ -1,7 +1,14 @@
 package com.cucumber.freamework.helper.pageObject.mainscreen;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -86,5 +93,35 @@ public class MainScreen {
 
 	@FindBy(how = How.ID, using = "button_9")
 	public MobileElement btn9;
+
+	public void installAndLaunchApplication() throws MalformedURLException {
+		new DesiredCapabilities();
+		DesiredCapabilities capabilities = DesiredCapabilities.android();
+		capabilities.setCapability("no", true);
+		capabilities.setCapability("newCommandTimeout", 100);
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability("appium-version", "1.10.0");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("platformVersion", "6.0");
+		capabilities.setCapability("deviceName", "A67PVGQCIZMBAMDQ");
+		capabilities.setCapability("app", "/Users/vaibhav.bhapkar/Downloads/Sudoku.apk");
+		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		driver.findElement(By.id("btnAccept")).click();
+		driver.findElement(By.id("btnAccept")).click();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		driver.findElement(By.id("skip")).click();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+	}
+
+	public void clickTheme() {
+		if (imgTheme.isDisplayed()) {
+			imgTheme.click();
+			log.info("");
+		} else {
+			log.info("Theme option does not displayed");
+			driver.close();
+		}
+	}
 
 }
